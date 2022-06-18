b1acfa0c8e18fb7bd6b28e3595361823a8ab9609
Stefan Prechtl <stefanmprechtl>
2022-06-18 13:47:02 +0200

    Anpassung an generischen Test

--- src/test/java/de/esempe/service/project/ProjectServiceTest.java
+++ src/test/java/de/esempe/service/project/ProjectServiceTest.java
@@ -5,22 +5,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class ProjectServiceTest
+import de.esempe.service.AbstractServiceTest;
+
+/**
+ * Integrationstest mit User-Service.
+ *
+ * @author Stefan Prechtl (www.esempe.de)
+ *
+ */
+@Tag("integration-test")
+@DisplayName("Integrationstest  UserService/REST-API Applikationsserver")
+public class ProjectServiceTest extends AbstractServiceTest<ProjectService>
 {
-	private ProjectService objUnderTest;
 
 	@BeforeEach
 	public void setUp()
 	{
-		// this.objUnderTest = new ProjectService();
+		super.setUp(ProjectService.class);
+	}
+
+	@Override
+	protected ProjectService createObjUnderTest()
+	{
+		return new ProjectService(this.registry, this.logger);
 	}
 
 	@Test
-	public void getProjects() throws IOException, InterruptedException
+	public void getEntities() throws IOException, InterruptedException
 	{
 		final var result = this.objUnderTest.loadAll();
 		assertThat(result).isNotNull();
+		assertThat(result).isNotEmpty();
 	}
+
 }
